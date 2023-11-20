3745d9cef212b5515994cec26f5752cbe4b9d329
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-11-20 17:45:19 +0100

    Data & Property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // selector: 'app-servers',
+  selector: 'app-servers',
   // selector: '[app-servers]',
-  selector: '.app-servers',
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.sass'
 })
 export class ServersComponent {
 
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer = true;
+    } ,2000);
+  }
+
+  ngOnInit() {}
 }
